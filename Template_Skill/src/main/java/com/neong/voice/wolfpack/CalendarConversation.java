2c5bc96253fd266203e029684dd50f9ff8ff4b88
Ryan Moeller <ryan@freqlabs.com>
2016-10-11 21:05:49 -0700

    Begin restructuring CalendarConversation class
    Lots of comments have been added, some variables renamed, and a few
    bits of code are broken out into their own functions.
    
    Our position in the list can be stored as an int in the session, but
    intents and code for changing position in the list have not been
    added yet, and we still return a tell response and end the session,
    rather than an ask response which keeps the session active.
    
    The first intent request we get in a session, the position session
    attribute is initialized to 0.  Later in the session, the position
    may be changed when the user expresses an intent to move forward or
    backward in the list.

--- Template_Skill/src/main/java/com/neong/voice/wolfpack/CalendarConversation.java
+++ Template_Skill/src/main/java/com/neong/voice/wolfpack/CalendarConversation.java
@@ -15,62 +15,114 @@ import java.time.ZonedDateTime;
 
 public class CalendarConversation extends Conversation {
 	// Intent names
-	private final static String INTENT_NEXTEVENT = "NextEventIntent";
+	private final static String NEXT_EVENT_INTENT = "NextEventIntent";
 
+	// Session attributes
+	private final static String CURSOR_POSITION_ATTRIB = "cursor_position";
+
+	// Date/time formatters
 	private final static ZoneId PST = ZoneId.of("America/Los_Angeles");
-	private final static DateTimeFormatter TIMEFORMATTER = DateTimeFormatter.ofPattern("h:mm a");
-	private final static DateTimeFormatter DAYFORMATTER = DateTimeFormatter.ofPattern("EEEE");
-	private final static DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("????MMdd");
+	private final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("h:mm a");
+	private final static DateTimeFormatter DAY_FORMATTER = DateTimeFormatter.ofPattern("EEEE");
+	private final static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("????MMdd");
 
+	// Database connection
 	private DbConnection db;
 
+
+	// Class constructor (note: we get constructed on every request)
 	public CalendarConversation() {
+		// Call the parent class constructor.
 		super();
 
+		// Create a database connection using the specified settings file.
 		db = new DbConnection("DbCredentials.xml");
-		db.getRemoteConnection();
 
 		// Add custom intent names for dispatcher use.
-		supportedIntentNames.add(INTENT_NEXTEVENT);
+		supportedIntentNames.add(NEXT_EVENT_INTENT);
 	}
 
 
+	// Intent request handler (this is our main entry point)
 	@Override
 	public SpeechletResponse respondToIntentRequest(IntentRequest intentReq, Session session) {
 		Intent intent = intentReq.getIntent();
 		String intentName = (intent != null) ? intent.getName() : null;
 		SpeechletResponse response = null;
 
-		if (INTENT_NEXTEVENT.equals(intentName))
+		// Get a connection to the database
+		if (!db.getRemoteConnection())
+			// We couldn't connect; fail gracefully
+			return internalErrorResponse();
+
+		// Dispatch to the specifc handler for the intent received
+		switch (intentName) {
+
+		case NEXT_EVENT_INTENT:
+			// Initialize the position to 0 if not already set.
+			if (session.getAttribute(CURSOR_POSITION_ATTRIB) == null)
+				session.setAttribute(CURSOR_POSITION_ATTRIB, 0);
+
 			response = handleNextEventIntent(intentReq, session);
+			break;
+
+		// TODO: handle cursor control intents (AMAZON.NextIntent, AMAZON.PreviousIntent)
+
+		default:
+			// This should never be reached; we only get intents we support
+			return internalErrorResponse();
+
+		}
 
 		return response;
 	}
 
 
+	// Handle a specific intent (TODO: this name doesn't make sense anymore)
 	private SpeechletResponse handleNextEventIntent(IntentRequest intentReq, Session session) {
 		Map<String, Vector<Object>> results =
-				db.runQuery("SELECT * FROM ssucalendar.event_info WHERE start > now() LIMIT 1;");
-
+			db.runQuery("SELECT * FROM ssucalendar.event_info WHERE start > now() LIMIT 1;");
 		if (results == null)
-			return newTellResponse("Sorry, I'm on break", false);
+			// The query failed for some reason (check the logs)
+			return internalErrorResponse();
 
-		String summary = (String) results.get("summary").get(0);
-		Timestamp start = (Timestamp) results.get("start").get(0);
-		String location = (String) results.get("name").get(0);
+		// State we want to persist between requests is stored in the session
+		int position = (int) session.getAttribute(CURSOR_POSITION_ATTRIB);
 
+		// TODO: this needs to be an ask response, to keep the session open
+		return newTellResponse(formatResponseSsml(position, results), true);
+	}
+
+
+	// "Pay no attention to that man behind the curtain!"
+	private static SpeechletResponse internalErrorResponse() {
+		return newTellResponse("Sorry, I'm on break.", false);
+	}
+
+
+	// Helper to extract info from results at position and format as SSML
+	private static String formatResponseSsml(int position, Map<String, Vector<Object>> results) {
+		// The "summary" is the title of the event.
+		String summary = (String) results.get("summary").get(position);
+
+		Timestamp start = (Timestamp) results.get("start").get(position);
+
+		// Location is optional on the calendars.
+		String location = (String) results.get("name").get(position);
 		if (location == null)
 			location = "Sonoma State University";
 
+		// Format the start timestamp into day of week, month+day, and 12-hour time strings.
 		ZonedDateTime zonedDateTime = start.toLocalDateTime().atZone(PST);
-		String date = zonedDateTime.format(DATEFORMATTER);
-		String day = zonedDateTime.format(DAYFORMATTER);
-		String time = zonedDateTime.format(TIMEFORMATTER);
+		String date = zonedDateTime.format(DATE_FORMATTER);
+		String day = zonedDateTime.format(DAY_FORMATTER);
+		String time = zonedDateTime.format(TIME_FORMATTER);
 
-		return newTellResponse("<speak> Okay, the next event is " + summary +
-				" on " + day +  " <say-as interpret-as=\"date\">" + date +
-				"</say-as> at <say-as interpret-as=\"time\">" + time + "</say-as> at "
-				+ location + ". </speak>", true);
+		// Return a message decorated with SSML tags.
+		return "<speak>Okay, the next event is " + summary +
+			" on " + day + " <say-as interpret-as=\"date\">" + date +
+			"</say-as> at <say-as interpret-as=\"time\">" + time +
+			"</say-as> at " + location + ".</speak>";
 	}
 
 }

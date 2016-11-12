98522bd6e4c8de406b45983323d278b634260440
erich <bucher.erich@gmail.com>
2016-11-11 16:33:19 -0800

    Add url to new calendar for academic dates

--- Database/calendar_urls.sql
+++ Database/calendar_urls.sql
@@ -7,4 +7,5 @@ INSERT INTO calendar_urls(url_text)
           ('https://25livepub.collegenet.com/calendars/Diversity_Related.ics'),
           ('https://25livepub.collegenet.com/calendars/Lectures_and_Films.ics'),
           ('https://25livepub.collegenet.com/calendars/Student_Calendar.ics'),
-          ('https://25livepub.collegenet.com/calendars/Classes_and_Workshops.ics');
+          ('https://25livepub.collegenet.com/calendars/Classes_and_Workshops.ics'),
+          ('https://s3.amazonaws.com/ssunewsbucket/ssunews_academic_calendar.ics');
